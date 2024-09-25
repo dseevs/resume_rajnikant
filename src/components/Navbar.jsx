b4d39ddcf11174e8d83e64a7853acdc1b79eb250
rajnikant <rajnikanttiwari100@gmail.com>
2024-09-25 18:59:14 +0530

    "latest change"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,28 @@
 import { NavLink } from "react-router-dom";
 
-import { home} from "../assets/images";
+import { home } from "../assets/images";
 
 const Navbar = () => {
   return (
-    <header className='header '>
-      <NavLink to='/'>
-        <img src={home} alt='logo' className=' img-fluid' style={{}}/>
+    <header className="header ">
+      <NavLink to="/">
+        <img src={home} alt="logo" className=" img-fluid" style={{}} />
       </NavLink>
-      <nav className='flex text-lg gap-7 font-medium'>
-        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
           About
         </NavLink>
-        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
           Projects
         </NavLink>
       </nav>
